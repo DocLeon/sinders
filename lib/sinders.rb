2b40f1edc155577f11821c28ba588007b0e19fdc
neilkidd <neil.a.kidd@gmail.com>
2015-11-17 20:16:19 +0000

    just foir fun

--- lib/sinders.rb
+++ lib/sinders.rb
@@ -1,5 +1,17 @@
-require "sinders/version"
+require 'sinders/version'
 
 module Sinders
-  # Your code goes here...
+  def self.check(id, seconds)
+    Thread.new { sleep seconds; reset_data_at id }
+  end
+
+  def self.reset_data_at(_id)
+  end
+
+  def self.transforms(data, till_midnight)
+  end
+
+  def self.get(_id)
+    { key: 'VALUE' }
+  end
 end
--- spec/sinders_spec.rb
+++ spec/sinders_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
 
 describe Sinders do
+  let(:data) { { key: 'value' } }
+  let(:till_midnight) { 0 }
+
   it 'has a version number' do
     expect(Sinders::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(false)
+  def create_new_instance
+    @id = subject.transforms(data, till_midnight) do |data|
+      data.each do |key, _value|
+        data.tap { |d| d[key].upcase! }
+      end
+    end
+  end
+
+  describe '.get(id)' do
+    context 'before midnight (before time expired)' do
+      it 'returns the transformed data' do
+        allow(Sinders).to receive(:check)
+        create_new_instance
+        expect(subject.get(@id)).to eq(key: 'VALUE')
+      end
+    end
   end
 end
